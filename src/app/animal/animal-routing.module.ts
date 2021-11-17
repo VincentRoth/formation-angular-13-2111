c446b09bba81d2ff8c7b136642327c8e29961894
Vincent Roth <vincent@placeme.io>
2021-11-17 10:01:33 +0100

    Routes enfants

--- src/app/animal/animal-routing.module.ts
+++ src/app/animal/animal-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnimalListComponent } from './animal-list/animal-list.component';
+import { AnimalRootComponent } from './animal-root/animal-root.component';
 import { AnimalComponent } from './animal/animal.component';
 
 const routes: Routes = [
-  { path: 'animals', component: AnimalListComponent },
-  { path: 'animals/:id', component: AnimalComponent },
+  {
+    path: 'animals',
+    component: AnimalRootComponent,
+    children: [
+      { path: '', component: AnimalListComponent },
+      { path: ':id', component: AnimalComponent },
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/animal/animal.module.ts
+++ src/app/animal/animal.module.ts
@@ -5,9 +5,10 @@ import { AnimalListComponent } from './animal-list/animal-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { AnimalItemComponent } from './animal-item/animal-item.component';
 import { AnimalRoutingModule } from './animal-routing.module';
+import { AnimalRootComponent } from './animal-root/animal-root.component';
 
 @NgModule({
-  declarations: [AnimalComponent, AnimalListComponent, AnimalItemComponent],
+  declarations: [AnimalComponent, AnimalListComponent, AnimalItemComponent, AnimalRootComponent],
   imports: [CommonModule, SharedModule, AnimalRoutingModule],
 })
 export class AnimalModule {}
