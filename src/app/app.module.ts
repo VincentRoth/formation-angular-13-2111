33f523f78f4be2641634edf061392783199704cc
Vincent Roth <vincent@placeme.io>
2021-11-18 16:06:38 +0100

    Internationalisation avec ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,17 @@ import { NotFoundModule } from './not-found/not-found.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(httpClient: HttpClient): TranslateLoader {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +25,13 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
     MatButtonModule,
     MatToolbarModule,
     AnimalModule,
@@ -23,4 +41,15 @@ import { MatButtonModule } from '@angular/material/button';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(translateService: TranslateService) {
+    translateService.setDefaultLang('fr');
+    translateService.langs = ['en', 'fr'];
+
+    if (
+      translateService.getLangs().includes(translateService.getBrowserLang())
+    ) {
+      translateService.use(translateService.getBrowserLang());
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { MailToDirective } from './mail-to.directive';
 import { FormatPhonePipe } from './format-phone.pipe';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TimeInterceptor } from './api/time.interceptor';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [MailToDirective, FormatPhonePipe],
   imports: [CommonModule, HttpClientModule],
-  exports: [MailToDirective, FormatPhonePipe],
+  exports: [MailToDirective, FormatPhonePipe, TranslateModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TimeInterceptor, multi: true },
   ],
