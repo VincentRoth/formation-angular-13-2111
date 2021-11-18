e097cae8dc4a58a15f6aa63d2a25f5e38b3529c8
Vincent Roth <vincent@placeme.io>
2021-11-18 16:29:11 +0100

    PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import {
 } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 export function HttpLoaderFactory(httpClient: HttpClient): TranslateLoader {
   return new TranslateHttpLoader(httpClient);
@@ -37,6 +39,12 @@ export function HttpLoaderFactory(httpClient: HttpClient): TranslateLoader {
     AnimalModule,
     // NotFoundModule toujours en dernier pour la route **
     NotFoundModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
